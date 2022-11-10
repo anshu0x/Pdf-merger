d7fca20a4bce486ba0c1defea59f788ec0e70536
anshu0x <anshusharma6327@gmail.com>
2022-11-10 22:04:00 +0530

    start script added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const path = require("path");
